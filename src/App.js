04a64de78f505261b533429260c8131b1a0cac03
CharlieNData <charlesejewers@gmail.com>
2022-08-28 19:23:36 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
